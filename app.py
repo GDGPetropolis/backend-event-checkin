34037621f3218f227428a154b5ca8983e03fcab2
Johnathan Fercher <johnathanfercher22@gmail.com>
2019-02-24 19:25:26 -0300

    remomvido marreta de porta

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ app.register_blueprint(person_controller, url_prefix='/api')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0')

6e3d9d9e1c9a988548f5667fe5c568b34ec1393b
John Fercher <johnathanfercher22@gmail.com>
2019-03-28 12:43:43 -0300

    Salvando coisas no host

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-#!/usr/bin/env bash
-sudo docker run -e DATABASE=GdgDatabase -e DATABASE_USER=GdgUser -e DATABASE_USER_PASSWORD=GdgPassword -e DATABASE_HOST=172.17.0.1 -e DATABASE_PORT=3307 -d -p 5000:5000 gdg-backend
+#!/bin/bash
+
+ACT_PATH=$(pwd)
+
+docker run -v "$ACT_PATH/assets:/app/assets" -v "$ACT_PATH/output:/app/output" -e DATABASE=GdgDatabase -e DATABASE_USER=GdgUser -e DATABASE_USER_PASSWORD=GdgPassword -e DATABASE_HOST=172.17.0.1 -e DATABASE_PORT=3307 -d -p 5000:5000 gdg-backend
